eb93cfe617e9b6c92394f138e63a4628d722fcfc
ESCG\530240 <530240@chrome.escg.ac.uk>
2023-06-07 16:14:31 +0100

    COUNTDOWN WORKS

---
+++ Game/Assets/Sun_Temple/Scripts/TimeManager.cs
@@ -0,0 +1,55 @@
+﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+
+    public float TotalTime  = 600;
+    public float TimeRemaining { get; private set; }
+
+    public static Action<float> OnAdjustTime;
+   
+
+    private void Awake()
+    {
+
+        TimeRemaining = TotalTime;
+    }
+
+
+    void OnEnable()
+    {
+        OnAdjustTime += AdjustTime;
+      
+    }
+
+    
+
+    private void AdjustTime(float delta)
+    {
+        TimeRemaining += delta;
+    }
+
+    private void SetTime(int time, bool isFinite)
+    {
+        TotalTime = time;
+        TimeRemaining = TotalTime;
+    }
+
+    void Update()
+    {
+  
+            TimeRemaining -= Time.deltaTime;
+            if (TimeRemaining <= 0)
+            {
+                TimeRemaining = 0;
+
+            }
+
+    }
+
+
+}
+
---
+++ Game/Assets/Sun_Temple/Scripts/TimerHUDManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class TimerHUDManager : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    TimeManager m_TimeManager;
+    
+    private void Start()
+    {
+        m_TimeManager = FindObjectOfType<TimeManager>();
+       
+
+
+   
+    }
+    
+    void Update()
+    {
+        
+            timerText.gameObject.SetActive(true);
+            int timeRemaining = (int) Math.Ceiling(m_TimeManager.TimeRemaining);
+            timerText.text = string.Format("{0}:{1:00}", timeRemaining / 60, timeRemaining % 60);
+ 
+       
+    }
+}
